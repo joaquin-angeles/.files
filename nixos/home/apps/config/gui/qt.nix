16207b97e4004153a31d81b6cf36c3d1c35b6cdc
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2026-02-21 09:50:46 +0800

    Cleanup

--- nixos/home/apps/config/gui/qt.nix
+++ nixos/home/apps/config/gui/qt.nix
@@ -1,4 +1,4 @@
-{ pkgs,  ... }:
+{ config, pkgs,  ... }:
 
 {
     # Qt theming
--- nixos/home/apps/packages/theme.nix
+++ nixos/home/apps/packages/theme.nix
@@ -17,7 +17,7 @@
         libsForQt5.qtstyleplugin-kvantum
         nwg-look
     ];
-     
+
     # Cursor theming
     home.pointerCursor = {
         gtk.enable = true;
@@ -28,62 +28,11 @@
     };
 
     # Vencord theme
-    # home.file.".config/vesktop/themes/midnight-gruvbox.css".source "${config.home.homeDirectory}/.files/vencord/themes/midnight-gruvbox.css"; # Native theming
-    home.file.".var/app/dev.vencord.Vesktop/config/vesktop/themes/midnight-gruvbox.css".source = "${config.home.homeDirectory}/.files/vencord/themes/midnight-gruvbox.css";
+    home.file = {
+        # ".config/vesktop/themes/midnight-gruvbox.css".source "${config.home.homeDirectory}/.files/vencord/themes/midnight-gruvbox.css"; 
+        ".var/app/dev.vencord.Vesktop/config/vesktop/themes/midnight-gruvbox.css".source = "${config.home.homeDirectory}/.files/vencord/themes/midnight-gruvbox.css";
+    };
 
     # Wallpapers
     home.file."Pictures/Wallpapers".source = "${config.home.homeDirectory}/.files/backgrounds";
-
-    # GTK theming
-    gtk = {
-        enable = true;
-        theme = {
-            name = "Gruvbox-Dark";
-            package = pkgs.gruvbox-gtk-theme;
-        };
-
-        # Enable dark theme
-        gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
-        gtk4.extraConfig.gtk-application-prefer-dark-theme = true;
-    };
-
-    # GTK4 dark theme
-    dconf = {
-        enable = true;
-        settings = {
-            "org/gnome/desktop/interface" = {
-                "color-scheme" = "prefer-dark";
-            };
-        };
-    };
-
-    # Qt theming
-    qt = {
-        enable = true;
-        platformTheme.name = "qtct";
-        style = {
-            name = "kvantum-dark";
-        };
-    };
-
-    # Overwritten config files
-    xdg.configFile."gtk-4.0/settings.ini".force = true;
-    xdg.configFile."gtk-4.0/assets".source = "${pkgs.gruvbox-gtk-theme}/share/themes/Gruvbox-Dark/gtk-4.0/assets";
-    xdg.configFile."gtk-4.0/gtk.css".source = "${pkgs.gruvbox-gtk-theme}/share/themes/Gruvbox-Dark/gtk-4.0/gtk.css";
-    xdg.configFile."gtk-4.0/gtk-dark.css".source = "${pkgs.gruvbox-gtk-theme}/share/themes/Gruvbox-Dark/gtk-4.0/gtk-dark.css";
-
-    # Flatpak GTK theme correction
-    home.activation.flatpakGtkThemes = config.lib.dag.entryAfter ["writeBoundary"] ''
-        # Correct directory
-        $DRY_RUN_CMD mkdir -p "$HOME/.local/share/themes"
-    
-        # Theme packages
-        for pkg in \
-            ${pkgs.gruvbox-gtk-theme} \
-            ${pkgs.adw-gtk3}
-        do
-            $DRY_RUN_CMD find "$pkg/share/themes" -mindepth 1 -maxdepth 1 -type d \
-                -exec ln -sfn {} "$HOME/.local/share/themes/" \;
-        done
-    '';
 }
