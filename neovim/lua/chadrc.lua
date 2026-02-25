fa5c52d15e8aaf76e06d2a9adb921597e80729cd
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2026-02-25 17:52:09 +0800

    Disabled NvDash

--- neovim/lua/chadrc.lua
+++ neovim/lua/chadrc.lua
@@ -1,6 +1,6 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
@@ -14,44 +14,56 @@ M.base46 = {
 }
 
 -- Dashboard
-M.nvdash = {
-    load_on_startup = true,
-
-    -- Modules
-    buttons = {
-        { txt = "󰈔 New File", keys = "fn", cmd = "enew" }, -- Make new file
-        { txt = " Find File", keys = "ff", cmd = "Telescope find_files" }, -- Fuzzy file search
-        { txt = " Recent Files", keys = "fo", cmd = "Telescope oldfiles" }, -- Recent file search (fuzzy)
-        { txt = "󰈭 Find Word", keys = "fw", cmd = "Telescope live_grep" }, -- Word search (fuzzy)
-        { txt = " Mappings", keys = "ch", cmd = "NvCheatsheet" }, -- Keybind cheatsheet
-        { txt = "󱥚 Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" }, -- Theme picker
-        { txt = "󰍃 Quit", keys = "q", cmd = "qa!" }, -- Make new file
-
-        { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
-
-        {
-            txt = function()
-                local stats = require("lazy").stats()
-                local ms = math.floor(stats.startuptime) .. " ms"
-                return " Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
-            end,
-            hl = "NvDashFooter",
-            no_gap = true,
-            content = "fit",
-        },
-
-        { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
-    },
-}
+-- M.nvdash = {
+--     load_on_startup = false,
+--     header = {
+--         "                                                                       ",
+--         "                                                                     ",
+--         "       ████ ██████           █████      ██                     ",
+--         "      ███████████             █████                             ",
+--         "      █████████ ███████████████████ ███   ███████████   ",
+--         "     █████████  ███    █████████████ █████ ██████████████   ",
+--         "    █████████ ██████████ █████████ █████ █████ ████ █████   ",
+--         "  ███████████ ███    ███ █████████ █████ █████ ████ █████  ",
+--         " ██████  █████████████████████ ████ █████ █████ ████ ██████ ",
+--         "                                                                       ",
+--     },
+-- 
+--     -- Modules
+--     buttons = {
+--         { txt = "󰈔 New File", keys = "fn", cmd = "enew" }, -- Make new file
+--         { txt = " Find File", keys = "ff", cmd = "Telescope find_files" }, -- Fuzzy file search
+--         { txt = " Recent Files", keys = "fo", cmd = "Telescope oldfiles" }, -- Recent file search (fuzzy)
+--         { txt = "󰈭 Find Word", keys = "fw", cmd = "Telescope live_grep" }, -- Word search (fuzzy)
+--         { txt = " Mappings", keys = "ch", cmd = "NvCheatsheet" }, -- Keybind cheatsheet
+--         { txt = "󱥚 Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" }, -- Theme picker
+--         { txt = "󰍃 Quit", keys = "q", cmd = "qa!" },
+-- 
+--         { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+-- 
+--         {
+--             txt = function()
+--                 local stats = require("lazy").stats()
+--                 local ms = math.floor(stats.startuptime) .. " ms"
+--                 return " Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+--             end,
+--             hl = "NvDashFooter",
+--             no_gap = true,
+--             content = "fit",
+--         },
+-- 
+--         { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+--     },
+-- }
 
 -- Bufferline
 M.ui = {
     cmp = {
         style = "blink",
     },
---     tabufline = {
---         lazyload = false
---     }
+    -- tabufline = {
+        --     lazyload = false
+        -- }
 }
 
-return M
+    return M
