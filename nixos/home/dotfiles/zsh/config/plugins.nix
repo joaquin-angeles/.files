d2d0851a2e3f6ac910b01c43c69b17829319f709
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2026-02-25 18:18:01 +0800

    Updated colors

--- nixos/home/dotfiles/zsh/config/plugins.nix
+++ nixos/home/dotfiles/zsh/config/plugins.nix
@@ -6,9 +6,4 @@
         autosuggestion.enable = true;     # Autosuggestions
         enableCompletion = true;          # Auto-completions
     };
-
-    # Extra plugins
-    home.packages = with pkgs; [
-        zsh-powerlevel10k # Fast prompt
-    ];
 }
