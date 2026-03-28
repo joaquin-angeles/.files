3dbfca8672563798d9834ea53240deb9f9fa0ba4
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2026-03-28 13:40:48 +0800

    Removed duplicated transparency

--- vim/lua/options.lua
+++ vim/lua/options.lua
@@ -9,7 +9,6 @@ o.relativenumber = true
 o.swapfile = false -- Disable swap file
 
 -- Transparent background
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
 vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
 o.termguicolors = true
