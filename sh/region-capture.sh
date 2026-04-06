a80455b87297a41984e7c99e246dd7dfdcdc7733
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2026-04-06 14:28:47 +0800

    Shrinked region-capture.sh

--- sh/region-capture.sh
+++ sh/region-capture.sh
@@ -1,25 +1,11 @@
 #!/usr/bin/env dash
 
-set -e
+trap 'pkill hyprpicker 2>/dev/null' EXIT
 
-checkRunning() {
-    sleep 1
-    while true; do
-        if [ "$(pgrep slurp | wc -m)" = "0" ]; then
-            pkill hyprpicker 2>/dev/null
-            exit
-        fi
-    done
-}
+if command -v hyprpicker >/dev/null 2>&1; then
+    hyprpicker -r -z &
+    sleep 0.2
+fi
 
-begin_grab() {
-    if command -v hyprpicker >/dev/null 2>&1; then
-        hyprpicker -r -z &
-        sleep 0.2
-    fi
-
-    geometry=$(slurp -d)
-    grim -g "$geometry" - | wl-copy --type image/png
-}
-
-begin_grab & checkRunning
+geometry=$(slurp -d) || exit 1
+grim -g "$geometry" - | wl-copy --type image/png
