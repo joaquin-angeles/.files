f0e55c27b32bbce2db057f3112962fce6b01635b
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2026-04-06 23:43:05 +0800

    Parfait firefox theme

--- nix/home/apps/programs.nix
+++ nix/home/apps/programs.nix
@@ -1,10 +1,9 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   imports = [
     ./programs/btop.nix # System monitor
     ./programs/hyprlock.nix
-    ./programs/librewolf.nix
     ./programs/neovim.nix # Text editor
     # ./programs/lutris.nix            # Game launcher
     ./programs/rmpc.nix
