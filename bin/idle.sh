fba9a718a368e216f3a5a4d75888f8a43304e481
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2026-04-09 20:20:09 +0800

    Made a simpler idle.sh

--- bin/idle.sh
+++ bin/idle.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env dash
 
-lock_cmd='pgrep -x hyprlock >/dev/null || hyprlock'
-
 swayidle -w \
-    timeout 300 "$lock_cmd" \
-    timeout 600 'wlopm --off "*"' \
-    timeout 900 'systemctl suspend' \
-    resume 'wlopm --on "*"; pkill -SIGUSR1 swayidle' \
-    before-sleep "$lock_cmd"
+    timeout 300 'hyprlock' \
+    timeout 600 'systemctl suspend' \
+    before-sleep 'hyprlock'
