b9a7a5bbd326ec2e213a914d95955b65a432ff41
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2026-04-09 21:21:25 +0800

    Swayidle services

--- bin/idle.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env dash
-
-swayidle -w \
-    timeout 300 'hyprlock' \
-    timeout 600 'systemctl suspend' \
-    before-sleep 'hyprlock'
---
+++ nix/home/services/swayidle.nix
@@ -0,0 +1,23 @@
+{ ... }:
+
+{
+  services.swayidle = {
+    enable = true;
+    timeouts = [
+      {
+        timeout = 300;
+        command = "${pkgs.swaylock-effects}/bin/swaylock -f";
+      }
+      {
+        timeout = 600;
+        command = "${pkgs.systemd}/bin/systemctl suspend";
+      }
+    ];
+    events = [
+      {
+        event = "before-sleep";
+        command = "${pkgs.hyprlock}/bin/hyprlock";
+      }
+    ];
+  };
+}
