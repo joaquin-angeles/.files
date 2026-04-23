c24875d8aa518de27348e924e992dc5248fe8141
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2026-04-23 22:31:12 +0800

    One more update

--- nix/home/packages/default.nix
+++ nix/home/packages/default.nix
@@ -3,12 +3,10 @@
 {
   imports = [
     # Packages
-    ./archives.nix
     ./cli.nix
     ./file-manager.nix
     ./media.nix
     ./scripts.nix
-    ./themes.nix
     ./wayland.nix
   ];
 }
--- nix/home/theming/default.nix
+++ nix/home/theming/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [
@@ -9,4 +9,32 @@
     ./icons.nix
     ./qt.nix
   ];
+
+  # Theme packages
+  home.packages = with pkgs; [
+    adw-gtk3
+    kdePackages.qt6ct
+    kdePackages.qtstyleplugin-kvantum
+    libnotify
+    libsForQt5.qt5ct
+    libsForQt5.qtstyleplugin-kvantum
+    nwg-look
+  ];
+
+  # Cursor theming
+  home.pointerCursor = {
+    gtk.enable = true;
+
+    # Cursor theme
+    name = "Bibata-Modern-Ice";
+    package = pkgs.bibata-cursors;
+    size = 24;
+
+    # XWayland cursor theming
+    x11.enable = true;
+    x11.defaultCursor = "Bibata-Modern-Ice";
+  };
+
+  # Wallpapers
+  home.file."Pictures/Wallpapers".source = "${config.home.homeDirectory}/.files/walls";
 }
