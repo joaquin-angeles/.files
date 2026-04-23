a0245e01cc39df55725d42c5f63a7f16d3635db5
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2026-04-23 20:22:50 +0800

    Folded in layout generation on River

--- river/conf.d/tags.sh
+++ river/conf.d/tags.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env dash
 
-mod="Super"
-
 i=1
 while [ "$i" -le 9 ]; do
     tags=$(( 1 << (i - 1) ))
-    riverctl map normal "$mod" "$i" set-focused-tags "$tags"
-    riverctl map normal "$mod+Shift" "$i" set-view-tags "$tags"
+    riverctl map normal "Super" "$i" set-focused-tags "$tags"
+    riverctl map normal "Super+Shift" "$i" set-view-tags "$tags"
     i=$(( i + 1 ))
 done
 
 # View/set for all tags
 all_tags=$(( (1 << 32) - 1 ))
-riverctl map normal "$mod" 0 set-focused-tags "$all_tags"
-riverctl map normal "$mod+Shift" 0 spawn 'river-sticky'
+riverctl map normal "Super" 0 set-focused-tags "$all_tags"
+riverctl map normal "Super+Shift" 0 spawn 'river-sticky'
