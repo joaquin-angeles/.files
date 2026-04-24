80cdad021361ec6a87f05232ed8448d392286b11
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2026-04-24 12:50:27 +0800

    Fixed Lf programs config

--- nix/home/programs/lf/default.nix
+++ nix/home/programs/lf/default.nix
@@ -9,31 +9,27 @@
       dragon-out = ''%${pkgs.xdragon}/bin/xdragon -a -x "$fx"'';
 
       open = ''
-        ${{
-          if (command -v bat >/dev/null 2>&1 && bat "$f" > /dev/null 2>&1) || cat "$f" > /dev/null 2>&1; then
-            nvim "$f"
-          else
-            case $(file --mime-type "$f" -bL) in
-              image/*) swayimg "$f" ;;
-              application/x-executable) "$f" ;;
-              *) xdg-open "$f" ;;
-            esac
-          fi
-        }}
+        if (command -v bat >/dev/null 2>&1 && bat "$f" > /dev/null 2>&1) || cat "$f" > /dev/null 2>&1; then
+          nvim "$f"
+        else
+          case $(file --mime-type "$f" -bL) in
+            image/*) swayimg "$f" ;;
+            application/x-executable) "$f" ;;
+            *) xdg-open "$f" ;;
+          esac
+        fi
       '';
 
       fzf_jump = ''
-        ${{
-          res="$(fzf --walker=file,dir,follow,hidden --walker-skip=.git,node_modules,.Trash)"
-          if [ -n "$res" ]; then
-            if [ -d "$res" ]; then
-              cmd="cd"
-            else
-              cmd="select"
-            fi
-            lf -remote "send $id $cmd \"$res\""
+        res="$(fzf --walker=file,dir,follow,hidden --walker-skip=.git,node_modules,.Trash)"
+        if [ -n "$res" ]; then
+          if [ -d "$res" ]; then
+            cmd="cd"
+          else
+            cmd="select"
           fi
-        }}
+          lf -remote "send $id $cmd \"$res\""
+        fi
       '';
     };
 
@@ -67,31 +63,28 @@
     };
 
     extraConfig =
-    let
-      previewer =
-        pkgs.writeShellScriptBin "pv.sh" ''
-        file=$1
-        w=$2
-        h=$3
-        x=$4
-        y=$5
+      let
+        previewer = pkgs.writeShellScriptBin "pv.sh" ''
+          file=$1
+          w=$2
+          h=$3
+          x=$4
+          y=$5
 
-        if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" =~ ^image ]]; then
-            ${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
-            exit 1
-        fi
+          if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" =~ ^image ]]; then
+              ${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
+              exit 1
+          fi
 
-        ${pkgs.pistol}/bin/pistol "$file"
+          ${pkgs.pistol}/bin/pistol "$file"
+        '';
+        cleaner = pkgs.writeShellScriptBin "clean.sh" ''
+          ${pkgs.kitty}/bin/kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
+        '';
+      in
+      ''
+        set cleaner ${cleaner}/bin/clean.sh
+        set previewer ${previewer}/bin/pv.sh
       '';
-      cleaner = pkgs.writeShellScriptBin "clean.sh" ''
-        ${pkgs.kitty}/bin/kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
-      '';
-    in
-    ''
-      set cleaner ${cleaner}/bin/clean.sh
-      set previewer ${previewer}/bin/pv.sh
-    '';
   };
-
-  # ...
 }
