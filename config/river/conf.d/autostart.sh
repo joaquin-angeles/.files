1b1c6cfda88aa0640b0954b0dae5ba09419d2104
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2026-04-25 14:23:25 +0800

    Updated River config a bit

--- config/river/conf.d/autostart.sh
+++ config/river/conf.d/autostart.sh
@@ -16,20 +16,3 @@ riverctl spawn 'swayidle -w \
     timeout 900 "systemctl suspend" \
     before-sleep "gtklock --daemonize" \
     after-resume "wlopm --on \"*\""'
-
-# Layout generator
-exec wideriver \
-    --border-width 2 \
-    --border-color-focused 0x879251cc \
-    --border-color-unfocused 0x56595acc \
-    --layout left \
-    --layout-alt monocle \
-    --stack even \
-    --count-master 1 \
-    --ratio-master 0.55 \
-    --inner-gaps 12 \
-    --outer-gaps 12 \
-    --smart-gaps \
-    --border-width-smart-gaps 0 \
-    --log-threshold info \
-    >> /tmp/wideriver.log 2>&1
