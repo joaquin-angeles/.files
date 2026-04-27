46caf0082e2364a3743fa7fb6af88b801c72a926
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2026-04-27 15:10:02 +0800

    Updated waybar icons

--- nix/home/programs/waybar/style.nix
+++ nix/home/programs/waybar/style.nix
@@ -4,6 +4,7 @@
   programs.waybar.style = ''
     @define-color black   #1d2021;
     @define-color gray    #56595a;
+    @define-color border  #32332f;
     @define-color white   #d4be98;
     @define-color red     #ea6962;
     @define-color green   #a9b665;
@@ -12,25 +13,27 @@
     @define-color magenta #d3869b;
     @define-color cyan    #89b482;
 
-    window#waybar {
+    * {
       font-family: sans-serif, "MonaspiceNe Nerd Font Propo";
       font-feature-settings: "tnum";
+    }
+
+    window#waybar {
       background-color: @black;
       padding-bottom: 1px;
-      border-bottom: 1px solid #32332f;
+      border-bottom: 1px solid @border;
       font-size: 12pt;
       color: @white;
     }
 
     #tags button {
-      font-family: "MonaspiceNe Nerd Font Propo";
-      font-size: 12.5pt;
+      font-size: 13pt;
       color: @gray;
       padding: 0 5pt;
       margin: 0 5pt;
       min-width: 10pt;
-      border-radius: 0;
       transition: none;
+      border-radius: 0;
     }
 
     /* Tag settings */
@@ -55,19 +58,9 @@
 
 
     /* Per-tag size adjustments */
-    #tags button:nth-child(1) {
-      margin-top: -1px;
-      padding-top: 1px;
-      font-size: 15pt;
+    #tags button:nth-child(4) {
+      font-size: 12.5pt;
     }
-    #tags button:nth-child(1).focused {
-      padding-top: 4px;
-    }
-
-    #tags button:nth-child(3) {
-      font-size: 13pt;
-    }
-
     #tags button:nth-child(5) {
       font-size: 14pt;
     }
@@ -75,10 +68,8 @@
     #tags button:nth-child(7),
     #tags button:nth-child(8),
     #tags button:nth-child(9) {
-      font-family: sans-serif, "MonaspiceNe Nerd Font Propo";
       font-size: 12pt;
       color: transparent;
-      transition: none;
       margin: 0 -99px;
       padding: 0;
     }
@@ -93,8 +84,6 @@
       padding: 0 4pt;
       margin: 0 4pt;
       min-width: 8pt;
-      border-radius: 0;
-      transition: none;
     }
 
     #tags button:nth-child(7).focused,
@@ -127,7 +116,7 @@
 
     #pulseaudio {
       font-size: 14pt;
-      margin: 0px 8pt 0 4pt;
+      margin: 0 8pt 0 4pt;
     }
 
     #battery {
@@ -152,9 +141,11 @@
     }
 
     #layout {
+      font-family: monospace;
       background-color: darker(@black);
-      border-left: 1px solid #32332f;
-      padding: 0 4px 0 4px;
+      color: @gray;
+      border-left: 1px solid @border;
+      padding: 0 4px;
     }
   '';
 }
