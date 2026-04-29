e02c75c368154d1b72f327d2f13670bdd8db6496
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2026-04-29 21:16:53 +0800

    Improved keybind readability on River

--- config/river/conf.d/30-keybinds.sh
+++ config/river/conf.d/30-keybinds.sh
@@ -1,77 +1,79 @@
 #!/usr/bin/env dash
 
 mod="Super"
+alias bind="riverctl map normal"
+alias bindm="riverctl map-pointer normal"
 
 # Tag management
 for i in $(seq 1 9); do
     tags=$((1 << (i - 1)))
-    riverctl map normal $mod "$i" set-focused-tags $tags
-    riverctl map normal $mod+Shift "$i" set-view-tags $tags
-    riverctl map normal $mod+Control "$i" toggle-focused-tags $tags
+    bind $mod "$i" set-focused-tags $tags
+    bind $mod+Shift "$i" set-view-tags $tags
+    bind $mod+Control "$i" toggle-focused-tags $tags
 done
 
 # View/set windows for all tags
 all_tags=$(( (1 << 32) - 1 ))
-riverctl map normal $mod 0 set-focused-tags "$all_tags"
-riverctl map normal $mod+Shift 0 spawn 'river-sticky'
+bind $mod 0 set-focused-tags "$all_tags"
+bind $mod+Shift 0 spawn 'river-sticky'
 
 # Layout switching
-riverctl map normal $mod space send-layout-cmd wideriver "--layout-toggle"
-riverctl map normal $mod t send-layout-cmd wideriver '--layout left'
-riverctl map normal $mod m send-layout-cmd wideriver '--layout monocle'
+bind $mod space send-layout-cmd wideriver "--layout-toggle"
+bind $mod t send-layout-cmd wideriver '--layout left'
+bind $mod m send-layout-cmd wideriver '--layout monocle'
 
 # Window management
-riverctl map normal $mod+Shift c close
-riverctl map normal $mod f toggle-fullscreen
-riverctl map normal $mod+Shift space toggle-float
-riverctl map normal $mod+Shift return zoom
+bind $mod+Shift c close
+bind $mod f toggle-fullscreen
+bind $mod+Shift space toggle-float
+bind $mod+Shift return zoom
 
 # Focus (jk)
-riverctl map normal $mod j focus-view next
-riverctl map normal $mod k focus-view previous
+bind $mod j focus-view next
+bind $mod k focus-view previous
 
 # Resize (hl)
-riverctl map normal $mod h send-layout-cmd wideriver '--ratio -0.05'
-riverctl map normal $mod l send-layout-cmd wideriver '--ratio +0.05'
-riverctl map normal $mod i send-layout-cmd wideriver '--count +1'
-riverctl map normal $mod d send-layout-cmd wideriver '--count -1'
-riverctl map-pointer normal $mod BTN_RIGHT resize-view
+bind $mod h send-layout-cmd wideriver '--ratio -0.05'
+bind $mod l send-layout-cmd wideriver '--ratio +0.05'
+bind $mod i send-layout-cmd wideriver '--count +1'
+bind $mod d send-layout-cmd wideriver '--count -1'
+bindm $mod BTN_RIGHT resize-view
 
 # Swap (Shift+hjkl)
-riverctl map normal $mod+Shift j swap next
-riverctl map normal $mod+Shift j swap previous
-riverctl map-pointer normal $mod BTN_LEFT move-view
+bind $mod+Shift j swap next
+bind $mod+Shift k swap previous
+bindm $mod BTN_LEFT move-view
 
 # Apps
-riverctl map normal $mod return spawn 'foot'
-riverctl map normal $mod p spawn 'rofi -show drun'
-riverctl map normal $mod w spawn 'librewolf'
-riverctl map normal $mod e spawn 'pcmanfm'
-riverctl map normal $mod period spawn 'rofi -no-show-icons -show emoji'
-riverctl map normal $mod v spawn 'cliphist list | rofi -dmenu -no-show-icons -p 󱘞 | cliphist decode | wl-copy'
+bind $mod return spawn 'foot'
+bind $mod p spawn 'rofi -show drun'
+bind $mod w spawn 'librewolf'
+bind $mod e spawn 'pcmanfm'
+bind $mod period spawn 'rofi -no-show-icons -show emoji'
+bind $mod v spawn 'cliphist list | rofi -dmenu -no-show-icons -p 󱘞 | cliphist decode | wl-copy'
 
 # App toggles
-riverctl map normal $mod b spawn 'pgrep waybar >/dev/null && pkill waybar || waybar'
-riverctl map normal $mod+Shift n spawn 'pgrep gammastep >/dev/null && pkill gammastep || gammastep -O 4000'
+bind $mod b spawn 'pgrep waybar >/dev/null && pkill waybar || waybar'
+bind $mod+Shift n spawn 'pgrep gammastep >/dev/null && pkill gammastep || gammastep -O 4000'
 
 # Screenshots
-riverctl map normal $mod s spawn 'grimshot --region'
-riverctl map normal $mod+Shift s spawn 'grimshot --output'
+bind $mod s spawn 'grimshot --region'
+bind $mod+Shift s spawn 'grimshot --output'
 
 # Misc
-riverctl map normal $mod+Shift q spawn 'rofi-power-menu'
-riverctl map normal $mod f1 spawn 'river-sheet'
+bind $mod+Shift q spawn 'rofi-power-menu'
+bind $mod f1 spawn 'river-sheet'
 
 # Volume & brightness
-riverctl map normal $mod minus spawn 'mako-osd --volume-down 5'
-riverctl map normal $mod equal spawn 'mako-osd --volume-up 5'
-riverctl map normal $mod bracketleft spawn 'mako-osd --brightness-down 5'
-riverctl map normal $mod bracketright spawn 'mako-osd --brightness-up 5'
-riverctl map normal $mod+Shift m spawn 'mako-osd --volume-mute'
+bind $mod minus spawn 'mako-osd --volume-down 5'
+bind $mod equal spawn 'mako-osd --volume-up 5'
+bind $mod bracketleft spawn 'mako-osd --brightness-down 5'
+bind $mod bracketright spawn 'mako-osd --brightness-up 5'
+bind $mod+Shift m spawn 'mako-osd --volume-mute'
 
 # Media keys
-riverctl map normal None XF86AudioLowerVolume spawn 'mako-osd --volume-down 5'
-riverctl map normal None XF86AudioRaiseVolume spawn 'mako-osd --volume-up 5'
-riverctl map normal None XF86AudioMute spawn 'mako-osd --volume-mute'
-riverctl map normal None XF86MonBrightnessUp   spawn 'mako-osd --brightness-up 5'
-riverctl map normal None XF86MonBrightnessDown spawn 'mako-osd --brightness-down 5'
+bind None XF86AudioLowerVolume spawn 'mako-osd --volume-down 5'
+bind None XF86AudioRaiseVolume spawn 'mako-osd --volume-up 5'
+bind None XF86AudioMute spawn 'mako-osd --volume-mute'
+bind None XF86MonBrightnessUp   spawn 'mako-osd --brightness-up 5'
+bind None XF86MonBrightnessDown spawn 'mako-osd --brightness-down 5'
--- nix/home/programs/fastfetch/default.nix
+++ nix/home/programs/fastfetch/default.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.fastfetch = {
     enable = true;
+    package = pkgs.fastfetchMinimal;
     settings = {
       "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
 
