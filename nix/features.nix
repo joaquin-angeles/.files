750a915f4e21bc0ece892738fc08ab5b80d6dac2
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2026-05-01 18:31:55 +0800

    Moved feature modules to appropriate files

--- nix/features.nix
+++ nix/features.nix
@@ -3,15 +3,22 @@
 with lib;
 
 {
+  imports = [
+    ./modules/gaming.nix
+    ./modules/webapps.nix
+  ];
+
   options.features = {
     gaming.enable = mkEnableOption "Gaming support";
     webapps.enable = mkEnableOption "Web applications";
   };
 
-  # Enable or disable gaming / webapps
-  # Change true -> false, vice versa
-  config.features = {
-    gaming.enable = true;
-    webapps.enable = true;
+  # Enable / disable features
+  # Change from true -> false to disable and vice versa
+  config = {
+    features = {
+      gaming.enable = false;
+      webapps.enable = true;
+    };
   };
 }
--- nix/home/packages/sets/default.nix
+++ nix/home/packages/sets/default.nix
@@ -4,7 +4,6 @@
   imports = [
     ./flatpaks.nix
     ./scripts.nix
-    ./webapps.nix
   ];
 
   home.packages = with pkgs; [
